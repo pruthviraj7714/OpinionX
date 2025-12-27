ea993256621394bfe87865b35c3901665c806ef4
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-12-27 23:15:34 +0530

    Basic Worker Logic Init

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,5 +1,42 @@
 import { Worker, Job } from "bullmq";
+import { prisma } from "@repo/db";
+import { redisclient } from "@repo/redisclient";
 
-const worker = new Worker("market-queue", async (job: Job) => {
-  const { marketId } = job.data;
-});
+new Worker(
+  "market-queue",
+  async (job: Job) => {
+    switch (job.name) {
+      case "close-market-on-expiry": {
+        const { marketId } = job.data;
+
+        const market = await prisma.market.findUnique({
+          where: {
+            id: marketId,
+          },
+        });
+
+        if (!market) {
+          throw new Error("Market not found!");
+        } else if (market.status === "CLOSED") {
+          throw new Error("Market is alreday closed");
+        } else if (market.status === "RESOLVED") {
+          throw new Error("Market is alreday resolved");
+        }
+
+        await prisma.market.update({
+          where: {
+            id: marketId,
+          },
+          data: {
+            status: "CLOSED",
+          },
+        });
+
+        break;
+      }
+    }
+  },
+  {
+    connection: redisclient,
+  }
+);
